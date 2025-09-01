0a3911a8bf130f9563060474ffaabc1c06250686
Stefan Frank <sfrsfr@gmail.com>
2025-09-01 21:27:28 +0200

    commented all lvgl8 unknown types

--- src/gui_toolkit/widgets/TabGroup.cpp
+++ src/gui_toolkit/widgets/TabGroup.cpp
@@ -59,6 +59,8 @@ void TabGroup::delTab(WidgetPtr tab) {
 }
 
 size_t TabGroup::getTabIndex(WidgetPtr tab) {
+    // FIXME
+    /*
     size_t cnt = lv_tabview_get_tab_count(obj());
     for (size_t i = 0; i < cnt; i++) {
         lv_obj_t *cur = lv_tabview_get_tab(obj(), i);
@@ -66,18 +68,25 @@ size_t TabGroup::getTabIndex(WidgetPtr tab) {
             return i;
         }
     }
+    */
     throw std::runtime_error("Tab not part of tab group");
 }
 
 void TabGroup::setActiveTab(size_t i) {
-    lv_tabview_set_tab_act(obj(), i, true);
+    // FIXME
+    //lv_tabview_set_tab_act(obj(), i, true);
 }
 
 size_t TabGroup::getActiveTab() {
-    return lv_tabview_get_tab_act(obj());
+    // FIXME
+    //return lv_tabview_get_tab_act(obj());
+    size_t a = 0;
+    return a;
 }
 
 void TabGroup::removeTab(size_t i) {
+    // FIXME
+    /*
     lv_tabview_ext_t *ext = reinterpret_cast<lv_tabview_ext_t *>(lv_obj_get_ext_attr(obj()));
 
     if (ext->tab_cnt <= 1) {
@@ -98,7 +107,6 @@ void TabGroup::removeTab(size_t i) {
 
     lv_obj_t *page = lv_tabview_get_tab(obj(), i);
     lv_obj_del(page);
-/*
     const lv_style_t * style_tabs = lv_obj_get_style(ext->btns);
     lv_coord_t indic_size = (lv_obj_get_width(obj()) - style_tabs->body.padding.inner * (ext->tab_cnt - 1) -
                  style_tabs->body.padding.left - style_tabs->body.padding.right) /
@@ -110,6 +118,7 @@ void TabGroup::removeTab(size_t i) {
     lv_tabview_set_btns_hidden(obj(), false);
     setActiveTab(ext->tab_cnt - 1);
     lv_obj_invalidate(obj());
+    */
 }
 
 void TabGroup::clear() {
