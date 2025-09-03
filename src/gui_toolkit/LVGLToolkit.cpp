4c4250ac330fad87f00a3713b898deb9a99d5d50
Stefan Frank <sfrsfr@gmail.com>
2025-09-03 12:26:13 +0200

    lv_task_handler is now lv_timer_handler

--- src/gui_toolkit/LVGLToolkit.cpp
+++ src/gui_toolkit/LVGLToolkit.cpp
@@ -202,7 +202,7 @@ void LVGLToolkit::guiLoop() {
 
         try {
             // first run the actual GUI tasks, i.e. let LVGL do its animations etc.
-            lv_task_handler();
+            lv_timer_handler();
 
             // then run our own tasks
             // To prevent race-conditions since a task could
